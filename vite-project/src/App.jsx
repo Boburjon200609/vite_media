782e43abdb0b4531794ef8be193cf1acdbf15bf6
Boburjon200609 <baxtiyorovboburjon22@gmail.com>
2024-12-09 07:43:11 +0500

    first commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='App'>
+        
+        <div className="medi">
+          <h1>media</h1>
+        </div>
+        <div className="medi">
+          <h1>media</h1>
+        </div>
+        <div className="medi">
+          <h1>media</h1>
+        </div>
+        <div className="medi">
+          <h1>media</h1>
+        </div>
+    </div>
+  )
+}
+
+export default App
